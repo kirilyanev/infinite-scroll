9940465ae97fa34ee0d9bac3af8dd7431fe670f0
Kiril Yanev <kl_yanev@abv.bg>
2024-08-28 16:49:08 +0300

    feat: implement fetch requests

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('testing')
+
+// Unsplash API
+const count = 10;
+const apiKey = 'o-1e7Kl5Fjo6SZWoOY-epVobX6VLYHtnh7ofvt5HmJg';
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Get photos from Unsplash API
+async function getPhotos() {
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        // Catch Error Here
+    }
+}
+
+// On Load
+getPhotos();
